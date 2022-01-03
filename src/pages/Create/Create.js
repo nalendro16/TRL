8eb6dc2e200165916a2c7ac91440b5ea91fdb4ac
nalendro16 <nalendro_agung@yahoo.com>
2022-01-04 00:39:33 +0700

    make create form better

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -1,5 +1,130 @@
+import { useState, useEffect } from 'react'
+import { useCollection } from '../../hooks/useCollection'
+import { timestamp } from '../../firebase/config'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import Select from 'react-select'
 import './Create.css'
 
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+]
+
 export default function Create() {
-  return <div>Create</div>
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+  const [errorForm, setErrorForm] = useState(null)
+
+  const { user } = useAuthContext()
+  const { documents } = useCollection('users')
+  // array for options
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map((user) => {
+        return { value: { ...user, id: user.id }, label: user.displayName }
+      })
+      setUsers(options)
+    }
+  }, [documents])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setErrorForm(null)
+
+    if (!category) {
+      setErrorForm('Please Select project Category')
+      return
+    }
+
+    if (assignedUsers.length < 1) {
+      setErrorForm('Please assigned one user (at leasts)')
+      return
+    }
+
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid,
+    }
+
+    const assignedUsersList = assignedUsers.map((usr) => {
+      return {
+        displayname: usr.value.displayName,
+        photoURL: usr.value.photoURL,
+        id: usr.value.id,
+      }
+    })
+
+    const project = {
+      name,
+      details,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      category: category.value,
+      comments: [],
+      createdBy,
+      assignedUsersList: assignedUsersList,
+    }
+
+    console.log(project)
+  }
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new Project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project Name:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project Details:</span>
+          <textarea
+            type="text"
+            required
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          />
+        </label>
+        <label>
+          <span>Set the Due Date:</span>
+          <input
+            type="date"
+            required
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project Category:</span>
+          <Select
+            required
+            options={categories}
+            onChange={(option) => setCategory(option)}
+          />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          <Select
+            options={users}
+            onChange={(option) => setAssignedUsers(option)}
+            isMulti
+          />
+        </label>
+        <button className="btn">Add Project</button>
+        {errorForm && <p className="error">{errorForm}</p>}
+      </form>
+    </div>
+  )
 }
